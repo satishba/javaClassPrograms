406719cd06f2f59babffbaf31b165d85574f29ed
satishba <satish.ashwath@gmail.com>
2022-10-29 10:08:47 +0530

    updated InputFromUser.java

--- unit1/InputFromUser.java
+++ unit1/InputFromUser.java
@@ -6,9 +6,19 @@ public class InputFromUser {
     public static void main(String args []){
 
         Scanner inputObj = new Scanner(System.in);
+
         System.out.println("Enter your id");
         int id = inputObj.nextInt();
-        System.out.println("User id is"+ id);
+
+       System.out.println("User id is"+ id);
+        float fl = inputObj.nextFloat();
+        System.out.println("Floating number is"+fl);
+        double db = inputObj.nextDouble();
+        System.out.println("Double number is"+db);
+        char ch = inputObj.next().charAt(0);
+        System.out.println("Char is "+ch);
+
+
 
 
     }
