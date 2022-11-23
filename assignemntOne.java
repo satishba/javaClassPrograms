4567ab993ec1b5bf550b45fd6f528b68b0819955
satishba <satish.ashwath@gmail.com>
2022-11-23 21:47:47 +0530

    assignment one

---
+++ assignemntOne.java
@@ -0,0 +1,56 @@
+package javaClassPrograms;
+
+import java.util.Scanner;
+
+class Student{
+
+    int usn;
+    char sem;
+    int marks[] = new int[8];
+    <TO DO:Add Constructor to initialize sem and usn>
+    <TO DO : Add constructor with no intializations>
+    public void setSem() {
+        Scanner sc  = new Scanner(System.in);
+        this.sem=sc.next().charAt(0);
+    }
+    public void setUsn(int usn) {
+        Scanner sc  = new Scanner(System.in);
+        this.usn=sc.nextInt();
+
+    }
+
+<TO DO : ADD METHOD TO SET MARKS>
+
+    public int[] getMarks() {
+        return marks;
+    }
+
+    public char getSem() {
+        return sem;
+    }
+
+    public int getUsn() {
+        return usn;
+    }
+    <TO DO: Add a method to find maximum marks>
+
+}
+
+
+public class assignemntOne {
+    public static void main(String[] args) {
+    Student eeeStudent1 = new Student();
+    int usn;
+    int sem;
+    Student eeeStudent2 = new Student(usn,sem);
+    <TO DO: Assign marks to students>
+
+    <TO DO: Print the max marks of both students>
+
+    <TO DO: Take input of a subject index from user and print
+    which student has more marks in that subject>
+
+
+    }
+}
+
--- unit2/basicEEEDept.java
+++ unit2/basicEEEDept.java
@@ -6,6 +6,7 @@ class Student{
 
     int usn;
     char sem;
+    int marks[] = new int[8];
     void welcome(){
         System.out.println("Welcome to EEE Dept");
     }
@@ -21,6 +22,14 @@ class Student{
 
     }
 
+    public void setMarks() {
+        this.marks = marks;
+    }
+
+    public int[] getMarks() {
+        return marks;
+    }
+
     public char getSem() {
         return sem;
     }
@@ -28,23 +37,98 @@ class Student{
     public int getUsn() {
         return usn;
     }
-}
 
+    void display(){
+        System.out.println("Welcom to "+ this.usn);
+    }
+
+    void display(int usn){
+        for(int x:marks){
+            System.out.println(x);
+        }
+    }
+
+    void display(int usn,int test){
+
+    }
+
+
+    int compare(Student temp){
+        float average1=0.0f,average2=0.0f;
+        for(int x:marks){
+            average1=average1+x;
+        }
+        average1=average1/8;
+
+        for(int x:temp.marks){
+            average2=average2+x;
+
+        }
+
+        average2=average2/8;
+
+    }
+}
+/*class Semester {
+    char sem;
+    int subjects;
+}*/
+class StudentWithMakrs{
+    int usn;
+    char sem;
+    int  marks[] = new int[10];
+
+    public void setMarks() {
+        for(int i=0;i<8;i++){
+            this.marks[i]=input.nextInt();
+        }
+    }
+
+    public void setUsn(int usn) {
+        usn=usn+1;
+        this.usn = usn;
+    }
+
+    public void setSem(char sem) {
+        this.sem = sem;
+    }
+
+    public int[] getMarks() {
+        return marks;
+    }
+}
 
 public class basicEEEDept {
     public static void main(String[] args) {
-        Student eeeStudent1,eeeStudent2,eeeStudent3;
-        eeeStudent1 = new Student();
-        char sem;
-        Scanner sc = new Scanner(System.in);
-        sem=sc.next().charAt(0);
+        StudentWithMakrs eeeStudent1;
+        eeeStudent1 = new StudentWithMakrs();
+        Scanner input = new Scanner(System.in);
+        int marks[]  = new int[6];
+        int i=0;
+        for(int x:marks){
+            marks[i++]=input.nextInt();
+        }
+        eeeStudent1.setMarks(marks);
 
-        eeeStudent1.setSem();
-        int usn;
-        usn = sc.nextInt();
-        eeeStudent1.setUsn(usn);
-        System.out.println(eeeStudent1.getSem());
-        System.out.println(eeeStudent1.getUsn());
+
+
+        Student eeeStudent2 = new Student();
+        eeeStudent2.setUsn(10);
+        eeeStudent2.setSem();
+        Scanner input1 = new Scanner(System.in);
+
+        eeeStudent2.setMarks();
+
+        Student eeeStudent3 = new Student();
+        int usn=10;
+        eeeStudent2.setUsn(usn);
+        eeeStudent2.setSem();
+        eeeStudent3.setMarks();
+
+        eeeStudent2.compare(eeeStudent3);
+
+
+
 
 
     }
