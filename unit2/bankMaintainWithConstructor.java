d20b90d1956c74fa88322ba34dabc9255cded375
satishba <satish.ashwath@gmail.com>
2022-11-29 22:55:14 +0530

    updated bank class

--- unit2/bankMaintainWithConstructor.java
+++ unit2/bankMaintainWithConstructor.java
@@ -1,12 +1,23 @@
-package javaClassPrograms.unit2;
+import java.util.Scanner;
+
 class Account1{
     int accountNumber;
     float accountBalance;
     int password;
 
+    int accountStatement[] = new int [10];
     Account1(int accountNumber,float accountBalance){
         this.accountNumber=accountNumber;
         this.accountBalance=accountBalance;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    void updateBalance(int value){
+        this.accountBalance = this.accountBalance + value;
+
     }
     public void setAccountNumber(int accountNumber) {
         this.accountNumber = accountNumber;
@@ -27,8 +38,32 @@ class Account1{
 public class bankMaintainWithConstructor {
     public static void main(String[] args) {
         Account1 account1,account2;
+        int num=0;
         account1 = new Account1(123,345.0f);
+        System.out.println("Do you want to witdraw or deposit");
+        Scanner input = new Scanner(System.in);
+        char choice=input.next().charAt(0);
+        if(choice == 'y'){
+            int password = input.nextInt();
+            boolean verify=verifyPassword(account1,password);
+            if(verify){
+                int value = input.nextInt();
+                account1.updateBalance(value);
 
+                account1.accountStatement[num++]=value;
+            }
+            else {
+                System.out.println("Invalid password");
+            }
+        }
 
+    }
+     public static boolean verifyPassword(Account1 acc, int givenPassword){
+        boolean correctPassword;
+        if(acc.password == givenPassword) {
+            return true;
+        }
+        else
+            return false;
     }
 }
